649a1bbd3c385e8adf9d6eab9c1242585f49d081
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-06 16:03:07 -0500

    added sattelites iteration

--- excercises/lists/planets.py
+++ excercises/lists/planets.py
@@ -12,7 +12,16 @@ planet_list.append("Pluto")
 rocky_planets = planet_list[0:4]
 # delete syntax for getting rid of poor pluto
 del planet_list[8]
-print(planet_list)
-print(rocky_planets)
+# print(planet_list)
+# print(rocky_planets)
 
 # ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+sats = [('Victory', 'Saturn'), ('Apollo', 'Venus'), ('Cassini', 'Mars')]
+
+def planets_with_sats():
+    for planet in planet_list:
+        for sat in sats:
+            if sat[1] == planet:
+                print(f'{sat[0]} is at {planet}')
+                
+planets_with_sats()                
