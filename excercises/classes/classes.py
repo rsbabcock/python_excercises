6ecc1c4986a2ef92f977e6bbb94569e3dc2d2ecc
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-06 15:37:00 -0500

    added nickelback

--- excercises/classes/classes.py
+++ excercises/classes/classes.py
@@ -4,6 +4,7 @@ class Company(object):
     def __init__(self, company_name, date_founded):
         self.company_name = company_name
         self.date_founded = date_founded
+        self.employees = set()
 
     def get_company_name(self):
         """Returns the name of the company"""
@@ -11,36 +12,32 @@ class Company(object):
         return self.company_name
 # Add the remaining methods to fill the requirements above
 
-
-# Create a class that contains information about employees 
-# of a company and define methods to get/set 
-# employee name, job title, and start date.
 class Employee(object):
     # this represents and employee
-    def __init__(self, first_name, last_name, job_title, start_date, employees):
+    def __init__(self, first_name, last_name, job_title, start_date):
         self.first_name = first_name
         self.last_name = last_name
         self.job_title = job_title
         self.start_date = start_date
-        self.employees = set()
+        
 
     def get_employee_name(self):
         # returns an employee
         return self.first_name + self.last_name
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # create company
-    NashvilleSoftwareSchool = Company("Nashville Software School", "2014")
+NashvilleSoftwareSchool = Company("Nashville Software School", "2014")
 
-    # create employees
-    steve = Employee("Steve", "Brownlee", "Instructor", "2015")
-    ryan = Employee("Ryan", "Tanay", "Instructor", "2015")
-    charisse = Employee("Charisse", "Lambert", "Instructor", "2015")
+# create employees
+steve = Employee("Steve", "Brownlee", "Instructor", "2015")
+ryan = Employee("Ryan", "Tanay", "Instructor", "2015")
+charisse = Employee("Charisse", "Lambert", "Instructor", "2015")
 
-    NashvilleSoftwareSchool.employees.add(steve)
-    NashvilleSoftwareSchool.employees.add(ryan)
-    NashvilleSoftwareSchool.employees.add(charisse)
-#     print(NashvilleSoftwareSchool)
+NashvilleSoftwareSchool.employees.add(steve)
+NashvilleSoftwareSchool.employees.add(ryan)
+NashvilleSoftwareSchool.employees.add(charisse)
+print(NashvilleSoftwareSchool.employees)
 
 # class Customer:
 #     def __init__(self, fn, ln, acct):
