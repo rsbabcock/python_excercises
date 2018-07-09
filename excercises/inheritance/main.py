8f7ab10bed0b5c94b71006e98a6fa50e7ff0a08c
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-09 13:59:22 -0500

    added correct instantiation chain

--- excercises/inheritance/main.py
+++ excercises/inheritance/main.py
@@ -1,6 +1,11 @@
 from dog import Dog
+from pet import Pet
+from animal import Animal
 # or import dog to get the whole file
 
 scout = Dog(breed="Aussie")
+maya = Pet("Maya", Dog("Aussie"))
+
+print(maya.__str__())
 
 print(scout.speak())
---
+++ excercises/inheritance/pet.py
@@ -0,0 +1,22 @@
+from animal import Animal
+from dog import Dog
+class Pet():
+
+    # animal_type = Dog("aussie")
+    #animal requires def __init__(self, species, leg_num=0, domesticated=False):
+    def __init__(self, name, critter_instance):
+        self.name = name
+        self.animal_type = critter_instance
+        # super().__init__("dog", leg_num=4, domesticated=True)
+
+    def assign_owner(self, owner):
+        self.owner = owner
+
+    def __str__(self):
+        return f'this pets name is {self.name}. It has {self.animal_type.leg_num} legs and likes to say {self.animal_type.say_something()}'
+
+# 1) Give a pet its own prop of name
+# 2) Give it a property of animal_type
+# 3) animal_type needs to be an instance of a dog
+# 4) assign an owner via a method
+# 5) __str__ return a string with pet's name, # of legs, and what it says    # 
