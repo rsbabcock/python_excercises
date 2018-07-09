0eb9aafef5153a12e6c1d461e0c3e9dbdf7803d3
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-09 11:58:42 -0500

    adding classes

--- excercises/inheritance/animal.py
+++ excercises/inheritance/animal.py
@@ -1,11 +1,13 @@
 class Animal():
+    
   def __init__(self, species, leg_num=0, domesticated=False):
     self.species = species
     self.leg_num = leg_num
     self.domesticated = domesticated
 
-  def saySomething(self):
+  def say_something(self):
+    print(self.species )
     if self.species == "dog":
-      return "Woof"
+      return "woof"
     else:
       return "moosqueakrrrrrchirp"
