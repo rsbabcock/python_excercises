eb816a04ba1febfe116e6822f6d368ae6fe219f1
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-10 11:02:51 -0500

    added steves dictionary code

--- excercises/dictionaries/stocks.py
+++ excercises/dictionaries/stocks.py
@@ -1,35 +1,75 @@
-# STOCK dictionary of ticker symbols and company names
-stockDict = { 'GM': 'General Motors', 'CAT':'Caterpillar', 'EK':"Eastman Kodak" }
-# list of tuples
-purchases = [ ( 'GM', 100, '10-sep-2001', 48 ), ( 'CAT', 100, '1-apr-1999', 24 ), ( 'GM', 200, '1-jul-1998', 56 ) ]
-# function syntax
-def purchase_history():
+stockDict = { 'GE': 'General Electric',
+ 'MSFT': 'Microsoft',
+ 'APPL': 'Apple',
+ 'CAT':'Caterpillar', 'EK':'Eastman Kodak' }
+
+purchases = [ ( 'GE', 100, '10-sep-2001', 48 ),
+ ( 'CAT', 100, '1-apr-1999', 24 ),
+ ( 'MSFT', 1000, '1-apr-1999', 198 ),
+ ( 'MSFT', 250, '1-apr-1999', 75 ),
+ ( 'MSFT', 333, '1-apr-1999', 100 ),
+ ( 'APPL', 50, '1-apr-1999', 144 ),
+ ( 'EK', 300, '5-apr-2017', 9 ),
+ ( 'GE', 200, '1-jul-1998', 56 ) ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+portfolio = dict()
+for purchase in purchases:
+    ticker = purchase[0]
+
+    full_company_name = stockDict[ticker]
+    number_of_shares = purchase[1]
+    purchase_price = purchase[3]
+    full_purchase_price = number_of_shares * purchase_price
+
+    minimal_purchase = (purchase[1], purchase[2], purchase[3])
+
+    try:
+        portfolio[ticker].append(purchase) # Append the purchase to the ticker list
+    except KeyError:
+        portfolio[ticker] = list() # If the key doesn't exist yet, create it
+        portfolio[ticker].append(purchase)
+
+
+    print("I purchased {} stock for ${}".format(full_company_name, full_purchase_price))
+
+print(portfolio)
+
+
+
+
+
+
+
+for ticker,purchases in portfolio.items():
+    print("------ {} ------".format(ticker))
+    total_portfolio_stock_value = 0
     for purchase in purchases:
-      for x in stockDict:
-        if purchase[0] == x:
-            print(f'{stockDict[purchase[0]]} + {purchase[1] * purchase[3]}')
-        
-purchase_history()
-
-# Create a second purchase summary that which accumulates total 
-# investment by ticker symbol. In the above sample data, there are two blocks of GE. 
-# These can easily be combined by creating a dict where the key is the ticker and the 
-# value is the list of blocks purchased. The program makes one pass through the data to
-# create the dict. A pass through the dict can then create a report showing each ticker 
-# symbol and all blocks of stock.
-
-# from levi shubert
-def report_by_ticker(list=purchases, stocks=stockDict):
-	# make an empty dict
-	tickers = {}
-	# stock is a variable name, like x, etc
-	for stock in list:
-		name = stocks[stock[0]]
-		value = stock[1]
-		if name in tickers.keys():
-			value += tickers[name]
-			print(value)
-		tickers.update({name: value})
-	return tickers
-
- report_by_ticker() 
+        total_portfolio_stock_value += purchase[1] * purchase[3]
+        print("    {}".format(purchase))
+    print("Total value of stock in portfolio: ${}\n\n".format(total_portfolio_stock_value))
