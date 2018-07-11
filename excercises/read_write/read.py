a88f16b51d42d1d751796d9a88ecca2b90543443
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-11 11:00:26 -0500

    added read write

--- excercises/read_write/read.py
+++ excercises/read_write/read.py
@@ -1,30 +1,30 @@
-# permissions: r: read only, w: write, a: appending, r+: read and write
-with open('test.txt', 'w') as testfile:
-    testfile.write("hello c25. Hope you're having a great day.")
+# permission oprions: r: read only, w: write only, a: appending, r+: read and write
+with open("test.txt", 'w') as testfile:
+  testfile.write("Hello, C25. Hope you're having a great day.")
 
-with open('test.txt', 'a') as testfile:
-    testfile.write("hello c25. Hope you're awake.")
+with open("test.txt", 'a') as testfile:
+  testfile.write("Hello, C25. Hope you're awake.")
 
-nickset = {"poop", 'Fatty'}
+nickset = {"The Mooch", "Knuckles", "Burninator", "Kneecap", "Ole Red", "Bubba",
+           "OG", "KitKat", "Spanky", "Monkeybutt", "L`il Devil", "Bird Person", "Fancy Slacks"}
 
-nameset = {"Bob", "Joe Bob"}
+nameset = {"Bob Smith", "Charise Anderson", "Jissie David", "Henry Goldberg", "Greg Korte", "Steve Brownlee", "Kimmy Bird", "Joe Shepherd", "Emily Lemmon", "Brenda Long", "Harold Brown", "Megan Ducharme", "Darth Vader"}
 
-with open("data/nickname.txt", "w") as nickname:
-    for nick in nickset:
-        nickname.write(nick + '\n')
+with open("data/nicknames.txt", "w") as nicknames:
+  for nick in nickset:
+    nicknames.write(nick + '\n')
 
 with open("data/names.txt", "w") as names:
-    for name in nameset:  
-        names.write(name + '\n')  
-# later, back at the batcave ...
+  for name in nameset:
+    names.write(name + '\n')
 
+# later, back at the batcave...``
 with open("data/names.txt", "r") as names:
-    namelist = names.readlines()
+  namelist = names.readlines()
 
 with open("data/nicknames.txt", "r") as nicks:
-    nicklist = nicks.readlines()
+  nicklist = nicks.readlines()
 
-mob_names = [f"{names.strip().split('')[0]}
-\"{nicklist[i].strip()}\"
-{name.strip().split(' ')[1]}
-for i, name in enumerate(namelist)"]
+mob_names = [f"{name.strip().split(' ')[0]} \"{nicklist[i].strip()}\" {name.strip().split(' ')[1]}" for i, name in enumerate(namelist)]
+
+print(mob_names)
