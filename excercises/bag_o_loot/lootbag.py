657c840b9b05856bcdbae15939d5e6f6e77fcec6
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-16 09:59:47 -0500

    added basic tests for bag-loot

--- excercises/bag_o_loot/lootbag.py
+++ excercises/bag_o_loot/lootbag.py
@@ -1,22 +1,20 @@
 class Bag():
 
-    def __init__(self):
-
-    # Items can be added to bag, and assigned to a child.
-        def add_item_to_bag_assign_to_child(self):
+         # Items can be added to bag, and assigned to a child.
+     def add_item_to_bag_assign_to_child(self):
             return True
-    # Items can be removed from bag, per child. 
-    #   Removing ball from the bag should not be allowed. 
-    #   A child's name must be specified.
-        def remove_item_from_bag_per_child(self):
+        # Items can be removed from bag, per child. 
+        #   Removing ball from the bag should not be allowed. 
+        #   A child's name must be specified.
+     def remove_item_from_bag_per_child(self):
             return True
-    # Must be able to list all children who are getting a toy.
-        def list_all_children_getting_toys(self):
+         # Must be able to list all children who are getting a toy.
+     def list_all_children_getting_toys(self):
             return True
-    # Must be able to list all toys for a given child's name.
-        def list_all_toys_for_specific_child(self):
+         # Must be able to list all toys for a given child's name.
+     def list_all_toys_for_specific_child(self):
             return True  
-    # Must be able to set the delivered property of a child, 
-    # which defaults to false to true.
-        def set_delivered_to_child(self):
+        # Must be able to set the delivered property of a child, 
+        # which defaults to false to true.
+     def set_delivered_to_child(self):
             return True
--- excercises/bag_o_loot/test_lootbag.py
+++ excercises/bag_o_loot/test_lootbag.py
@@ -6,13 +6,22 @@ class TestLootbag(unittest.TestCase):
   @classmethod
   def setUpClass(self):
     print('Set up class')
-    self.test_loot = Bag()
-    # Create an instance of the calculator that can be used in all tests
+    self.loot = Bag()
+    # Create an instance of the loot that can be used in all tests
 
   def test_add_item_to_bag_assign_to_child(self):
-    self.assertTrue(self.test_loot)
+    self.assertTrue(self.loot.add_item_to_bag_assign_to_child)
  
-  # Write test methods for subtract, multiply, and divide
+  def test_remove_item_from_bag_per_child(self):
+    self.assertTrue(self.loot.remove_item_from_bag_per_child)
 
+  def test_list_all_children_getting_toys(self):
+    self.assertTrue(self.loot.list_all_children_getting_toys)
+
+  def test_list_all_toys_for_specific_child(self):
+    self.assertTrue(self.loot.list_all_toys_for_specific_child)  
+
+  def test_set_delivered_to_child(self):
+    self.assertTrue(self.loot.set_delivered_to_child)
 if __name__ == '__main__':
     unittest.main()
